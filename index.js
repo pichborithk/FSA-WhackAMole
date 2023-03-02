acd5f3ab852289c03e3f433c82855159e8bc51dd
Pichborith Kong <pichborith@gmail.com>
2023-03-01 18:45:18 -0800

    add function to randomly pop up mole and update score when get hit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 let score = 0;
+const holes = document.querySelectorAll('.hole');
+const gameArea = document.getElementById('whack-a-mole');
+const scoreDisplay = document.getElementById('score');
+
+function getRandomHole() {
+  return Math.floor(Math.random() * holes.length);
+}
+
+function randomPopUp() {
+  const index = getRandomHole();
+  //   console.log(index);
+  holes[index].classList.toggle('mole');
+}
+
+function getHit(event) {
+  //   console.log(event.target);
+  if (event.target.matches('.mole')) {
+    score++;
+    event.target.classList.remove('mole');
+    scoreDisplay.innerText = score;
+  }
+}
+
+gameArea.addEventListener('click', getHit);
+// setInterval(randomPopUp, 300);
